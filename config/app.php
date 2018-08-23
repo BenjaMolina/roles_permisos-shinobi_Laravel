ed7fea03d3d06611662a110405d632cb966972c7
BenjaMolina <molinasb.94@gmail.com>
2018-08-23 17:24:49 -0500

    Fixed Clase Shinobi no encontrada y Ocultar botones si no existen permisos (restriccion visual)

--- config/app.php
+++ config/app.php
@@ -232,6 +232,8 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
 
+        'Shinobi' => Caffeinated\Shinobi\Facades\Shinobi::class,
+
     ],
 
 ];
